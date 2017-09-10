a5c98fc987a32527c4ab35e547d665ab8c26c8a8
ahyanarizky <ahyana.rizky@jurnal.id>
2017-09-10 09:56:11 +0700

    add styling to welcome page

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Cabinet
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Enable the asset pipeline
   end
 end
